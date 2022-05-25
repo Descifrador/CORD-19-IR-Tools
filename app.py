e660f9897a804d45ec74c38c4de1aa36645056dc
Harry Saini <harsimrans008@gmail.com>
2022-05-25 23:55:11 +0530

    Added everything

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import json
+from markupsafe import escape
+from search import semantic_search
+
+app = Flask(__name__)
+
+@app.route("/search", methods=["GET"])
+def search():
+
+   query = request.args.get("searchdata")
+   if query is not None :
+      df_res = semantic_search.search(request.args.get("searchdata"), request.args.get('category'))
+      json_res = df_res.to_json(orient="split")
+      search_results = json.loads(json_res)
+
+      return render_template("search.html", query=query, papers=search_results["data"], columns=search_results["columns"], category=request.args.get('category'))
+   
+   else :
+       return render_template("search.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
